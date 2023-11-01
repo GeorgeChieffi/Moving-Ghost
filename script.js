c035c1b1c4b47ddf24b5e62c00a54db2b5c7ad84
George Chieffi <georgechieffi@MacBook-Air-som-tillhor-George.local>
2023-11-01 13:49:55 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let ghost = document.querySelector(".ghost");
+//initialize X position
+ghost.style.left = window.innerWidth / 2 + "px";
+
+function handleClick(event) {
+    console.log(event)
+    let currentX = parseFloat(ghost.style.left);
+
+    ghost.dataset.moving = "false"
+
+    if (currentX > event.x) {
+        ghost.dataset.moving = "left";
+    }
+    if (currentX , event.x) {
+        ghost.dataset.moving = "right";
+    }
+
+    ghost.style.top = event.y + "px";
+    ghost.style.left = event.x + "px";
+}
+
+window.onclick = handleClick;
